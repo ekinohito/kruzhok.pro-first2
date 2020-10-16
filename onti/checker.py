a6fa7dbdc4174d929ba5e1b64b4b090cfa2f5942
ekinohito <zhenyagur9@gmail.com>
2020-10-16 13:45:35 +0300

    added test checker

--- onti/checker.py
+++ onti/checker.py
@@ -1,3 +1,5 @@
+import os
+
 from PIL import Image
 import cv2
 import pytesseract as tes
@@ -5,6 +7,15 @@ import pytesseract as tes
 
 def test():
     image = cv2.imread('test.png')
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # image = cv2.medianBlur(image, 3)
+    # image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    temporary_file_name = '.temp.png'
+    cv2.imwrite(temporary_file_name, image)
+
+    text = tes.image_to_string(Image.open(temporary_file_name), lang='rus')
+    # os.remove(temporary_file_name)
+    print(text)
 
 
 if __name__ == '__main__':
