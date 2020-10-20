2111598bd644e353c94a0bab50712faa51239f55
Your Name <you@example.com>
2020-10-20 19:21:18 +0300

    adding tests

--- onti/tests/applied_test.py
+++ onti/tests/applied_test.py
@@ -1,9 +1,10 @@
 import unittest
-from onti.checker import load_requirements, RequiredBlock, prepare
+from onti.checker import load_requirements, RequiredBlock, prepare, process_image
 import os
 import pytesseract
 from PIL import Image
 # pytesseract.pytesseract.tesseract_cmd = os.path.join('C:', 'Program Files', 'Tesseract-ocr', 'tesseract.exe')
+# pytesseract.pytesseract.tesseract_cmd = r'D:\tesseract\tesseract.exe'
 # pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 
@@ -32,6 +33,15 @@ class Form1Test(unittest.TestCase):
         data = pytesseract.image_to_data(im, lang='rus', output_type=pytesseract.Output.DICT)
         text = prepare(''.join(data['text']))
         self.assertEqual(self.req.compare(text, data, im), ('', False))
+
+    def test_block_compare4(self):
+        self.assertEqual(process_image('../test.png', self.req), ('', False))
+
+    def test_block_compare5(self):
+        self.assertEqual(process_image('../test2.png', self.req), ('', False))
+
+    def test_block_compare6(self):
+        self.assertEqual(process_image('../test3.jpg', self.req), ('', False))
 
 
 if __name__ == '__main__':
