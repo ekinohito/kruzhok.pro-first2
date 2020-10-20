9313e89e846b880330ef73f8954282ad66830fbf
ekinohito <zhenyagur9@gmail.com>
2020-10-20 17:36:39 +0300

    upgraded start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ python3 -m venv .
 source bin/activate
 cd ../
 sudo apt install tesseract-ocr
+sudo apt install tesseract-ocr-rus
 pip3 install numpy
 pip3 install opencv-python
 pip3 install Pillow
